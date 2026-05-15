c7d349c8d67c24449b2ab1d5821c676195bf416a
matisgod <matis.godet.2002@gmail.com>
2026-05-15 17:14:25 +0100

    Finished tasks and linting

--- marine_experiments/api.py
+++ marine_experiments/api.py
@@ -1,11 +1,15 @@
 """An API for handling marine experiments."""
 
-from datetime import datetime
-
 from flask import Flask, jsonify, request
-from psycopg2 import sql
 
-from database_functions import get_db_connection
+from database_functions import (
+    get_db_connection,
+    get_all_experiments,
+    get_experiments_by_score,
+    get_experiments_by_type,
+    get_experiments_by_score_and_type,
+    delete_experiment_by_id
+)
 
 
 app = Flask(__name__)
@@ -20,7 +24,26 @@ If you do not understand this instructions; as a coach to explain
 """
 conn = get_db_connection("marine_experiments")
 
+def check_experiment_type(experiment_type: str):
+    """Checks that the experiment type is allowed."""
+    if not isinstance(experiment_type,str):
+        return False
+    return experiment_type.lower() in [
+        "intelligence",
+        "obedience",
+        "aggression"
+    ]
 
+def check_experiment_score(experiment_score: int):
+    """Checks that the experiment score is allowed."""
+    if not isinstance(experiment_score,str):
+        return False
+    try:
+        experiment_score = int(experiment_score)
+    except ValueError:
+        return False
+    valid_experiment = 0 <= experiment_score <= 100
+    return valid_experiment
 @app.get("/")
 def home():
     """Returns an informational message."""
@@ -30,8 +53,36 @@ def home():
         "status": "Classified"
     })
 
+@app.get("/experiment")
+def get_experiments():
+    """Returns experiments. Can be filtered by type and score."""
+    experiment_type = request.args.get("type")
+    min_score = request.args.get("score_over")
+    if experiment_type and not check_experiment_type(experiment_type):
+        return {"error":"Invalid value for \'type\' parameter"}, 400
 
+    if min_score and not check_experiment_score(min_score):
+        return {"error":"Invalid value for \'score_over\' parameter"}, 400
+    if not experiment_type and not min_score:
+        experiments = get_all_experiments(conn)
 
+    elif experiment_type and not min_score:
+        experiment_type = experiment_type.lower()
+        experiments = get_experiments_by_type(conn,experiment_type)
+    elif not experiment_type and min_score:
+        experiments = get_experiments_by_score(conn,min_score)
+    else:
+        experiment_type = experiment_type.lower()
+        experiments = get_experiments_by_score_and_type(conn,experiment_type,min_score)
+    return experiments, 200
+
+@app.delete("/experiment/<int:experiment_id>")
+def delete_experiment(experiment_id: int) -> dict:
+    """Deletes an experiment by its id."""
+    deleted_experiment = delete_experiment_by_id(conn,experiment_id)
+    if deleted_experiment is None:
+        return {"error": f"Unable to locate experiment with ID {experiment_id}."}, 404
+    return deleted_experiment, 200
 if __name__ == "__main__":
     app.config["DEBUG"] = True
     app.config["TESTING"] = True
--- marine_experiments/database_functions.py
+++ marine_experiments/database_functions.py
@@ -14,3 +14,124 @@ def get_db_connection(dbname,
                    port=5432,
                    password=password,
                    cursor_factory=RealDictCursor)
+
+def get_all_experiments(conn:connection) -> list[dict]:
+    """Returns all experiments."""
+    cursor = conn.cursor()
+    query = """
+SELECT 
+    TO_CHAR(experiment_date,'YYYY-MM-DD') as experiment_date,
+    experiment_id,
+    type_name as experiment_type,
+    ROUND(score/max_score*100,2) as score,
+    species_name as species,
+    subject_id
+FROM experiment
+JOIN experiment_type
+    USING (experiment_type_id)
+JOIN subject
+    USING (subject_id)
+JOIN species 
+    USING (species_id)
+ORDER BY experiment_date DESC
+;
+"""
+    cursor.execute(query)
+    experiments = cursor.fetchall()
+    cursor.close()
+    return experiments
+
+def get_experiments_by_type(conn:connection, experiment_type: int) -> dict:
+    """Returns an experiment with matching type."""
+    cursor = conn.cursor()
+    query = """
+SELECT 
+    TO_CHAR(experiment_date,'YYYY-MM-DD') as experiment_date,
+    experiment_id,
+    type_name as experiment_type,
+    ROUND(score/max_score*100,2) as score,
+    species_name as species,
+    subject_id
+FROM experiment
+JOIN experiment_type
+    USING (experiment_type_id)
+JOIN subject
+    USING (subject_id)
+JOIN species 
+    USING (species_id)
+WHERE type_name = %s
+ORDER BY experiment_date DESC
+;"""
+    cursor.execute(query,(experiment_type,))
+    experiments = cursor.fetchall()
+    cursor.close()
+    return experiments
+
+def get_experiments_by_score(conn: connection, score: int) -> list[dict]:
+    """Returns all the experiments above a base score"""
+    cursor = conn.cursor()
+    query = """
+SELECT 
+    TO_CHAR(experiment_date,'YYYY-MM-DD') as experiment_date,
+    experiment_id,
+    type_name as experiment_type,
+    ROUND(score/max_score*100,2) as score,
+    species_name as species,
+    subject_id
+FROM experiment
+JOIN experiment_type
+    USING (experiment_type_id)
+JOIN subject
+    USING (subject_id)
+JOIN species 
+    USING (species_id)
+WHERE ROUND(score/max_score*100,2) > %s
+ORDER BY experiment_date DESC
+;"""
+    cursor.execute(query,(score,))
+    experiments = cursor.fetchall()
+    cursor.close()
+    return experiments
+
+def get_experiments_by_score_and_type(
+        conn: connection,
+        experiment_type: str,
+        score: int) -> list[dict]:
+    """Returns all the experiments of a specific type and above a base score"""
+    cursor = conn.cursor()
+    query = """
+SELECT 
+    TO_CHAR(experiment_date,'YYYY-MM-DD') as experiment_date,
+    experiment_id,
+    type_name as experiment_type,
+    ROUND(score/max_score*100,2) as score,
+    species_name as species,
+    subject_id
+FROM experiment
+JOIN experiment_type
+    USING (experiment_type_id)
+JOIN subject
+    USING (subject_id)
+JOIN species 
+    USING (species_id)
+WHERE ROUND(score/max_score*100,2) > %s
+AND type_name = %s
+ORDER BY experiment_date DESC
+;"""
+    cursor.execute(query,(score,experiment_type))
+    experiments = cursor.fetchall()
+    cursor.close()
+    return experiments
+
+def delete_experiment_by_id(conn: connection, experiment_id: int)-> dict:
+    """Deletes an experiment by its id."""
+    cursor = conn.cursor()
+    query = """
+DELETE FROM experiment
+WHERE experiment_id = %s
+RETURNING experiment_id,TO_CHAR(experiment_date,'YYYY-MM-DD') as experiment_date"""
+    cursor.execute(query,(experiment_id,))
+    deleted_experiment = cursor.fetchone()
+    conn.commit()
+    cursor.close()
+    return deleted_experiment
